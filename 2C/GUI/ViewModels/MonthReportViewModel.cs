e8dfc331482e4be764789732575be013d02a43bc
Анатолий <reloader2003@gmail.com>
2018-05-17 06:42:07 +0700

    Начал делать Вьюмодели для отчетов

---
+++ 2C/GUI/ViewModels/MonthReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GUI.BaseViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModels
+{
+    public class MonthReportViewModel : VmBase
+    {
+        private int _month;
+
+        public ArrivalsViewModel Arrivals { get; set; }
+        public int Month { get => _month; set => SetProperty(ref _month, value); }
+    }
+}
---
+++ 2C/GUI/ViewModels/ReportViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Core;
+using GUI.BaseViewModels;
+using GUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.ViewModels
+{
+    public class ReportViewModel : VmBase, IShopWorker
+    {
+        public ReportViewModel(ShopManager shopManger)
+        {
+            Title = "Годовой отчет";
+            ShopManager = ShopManager;
+        }
+
+        public ShopManager ShopManager { get; private set; }
+    }
+}
